6b2fd03c6c304ff02787760b50bd72349b0108f1
niqueborges <95103547+niqueborges@users.noreply.github.com>
2026-03-25 20:02:19 -0300

    fix: correct user id generation with uuid4

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from datetime import date
 from uuid import UUID, uuid4
 
 
 class User(BaseModel):
-    id: UUID = uuid4()
+    id: UUID = Field(default_factory=uuid4)
     name: str
     email: EmailStr
     birth_date: date
