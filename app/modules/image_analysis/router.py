28fd5ca13dd0f560f1c084dfaf25fb0af960a633
niqueborges <95103547+niqueborges@users.noreply.github.com>
2026-04-15 17:00:48 -0300

    feat: return image with face bounding boxes

--- app/modules/image_analysis/router.py
+++ app/modules/image_analysis/router.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
 import cv2
@@ -6,34 +8,48 @@ import numpy as np
 
 router = APIRouter(prefix="/image", tags=["Image Analysis"])
 
-face_cascade = cv2.CascadeClassifier(
-    "app/models/haarcascade_frontalface_default.xml"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+cascade_path = os.path.join(
+    BASE_DIR,
+    "models",
+    "haarcascade_frontalface_default.xml"
 )
+
+face_cascade = cv2.CascadeClassifier(cascade_path)
 
 
 @router.post("/analyze")
 async def analyze_image(file: UploadFile = File(...)):
     contents = await file.read()
 
-    image = Image.open(io.BytesIO(contents))
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
     image_np = np.array(image)
 
     gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=7,  
+    minSize=(50, 50)  
+)
 
-    formatted_faces = []
-
+    # desenhar retângulos
     for (x, y, w, h) in faces:
-        formatted_faces.append({
-            "x": int(x),
-            "y": int(y),
-            "width": int(w),
-            "height": int(h)
-        })
+        cv2.rectangle(
+            image_np,
+            (x, y),
+            (x + w, y + h),
+            (0, 255, 0),
+            2
+        )
 
-    return {
-        "filename": file.filename,
-        "faces_detected": len(formatted_faces),
-        "faces": formatted_faces
-    }
+    # converter de volta para imagem
+    result_image = Image.fromarray(image_np)
+
+    img_bytes = io.BytesIO()
+    result_image.save(img_bytes, format="JPEG")
+    img_bytes.seek(0)
+
+    return StreamingResponse(img_bytes, media_type="image/jpeg")
