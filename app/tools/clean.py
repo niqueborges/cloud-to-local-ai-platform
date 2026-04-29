92ef42d0e479a20a2e1058e180600ca9b59b94b6
niqueborges <95103547+niqueborges@users.noreply.github.com>
2026-04-29 12:40:15 -0300

    refactor(clean.py): simplificar lógica de remoção de pastas __pycache__

--- app/tools/clean.py
+++ app/tools/clean.py
@@ -6,11 +6,9 @@ def clean_cache() -> None:
     """Remove pastas __pycache__ ."""
     cache_dirs = ["__pycache__"]
     for root, dirs, _ in os.walk("."):
-        [
-            shutil.rmtree(os.path.join(root, cache_dir))
-            for cache_dir in cache_dirs
-            if cache_dir in dirs
-        ]
+        for cache_dir in cache_dirs:
+            if cache_dir in dirs:
+                shutil.rmtree(os.path.join(root, cache_dir))
 
 if __name__ == "__main__":
     clean_cache()
