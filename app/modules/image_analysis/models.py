c8a57e296545d506bd49a0cef42d5f8d0d388942
niqueborges <95103547+niqueborges@users.noreply.github.com>
2026-07-01 10:02:15 -0300

    feat(db): add status column to ImageAnalysis model

--- app/modules/image_analysis/models.py
+++ app/modules/image_analysis/models.py
@@ -9,6 +9,7 @@ class ImageAnalysis(Base):
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, nullable=False)
     path = Column(String, nullable=False)
-    faces_detected = Column(Integer, nullable=False)
+    status = Column(String, default="PENDING")
+    faces_detected = Column(Integer, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     user_id = Column(String, ForeignKey("users.id"), nullable=False)
